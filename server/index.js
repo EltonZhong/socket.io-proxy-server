bab7c371650bf47b2ce4381004e9080ff44e116f
elton.zhong <elton.zhong@ringcentral.com>
2019-02-13 15:13:08 +0800

    New version published: 1.4.52

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@
  * @returns SocketsManager
  * Usage: Modify packet with a reqHandler, and packet from client will be modified.
  * Usage: Modify packet with a respHandler, and packet from server will be modified.
+ * 
+ * Below is the example:
+ * ===========================================================
  * manager.addReqHandler(async function(proxySocket, packet) {
           console.log('req');
           console.log(arguments);
@@ -12,6 +15,7 @@
           console.log('resp');
           console.log(arguments);
      });
+ * ===========================================================
  */
 module.exports = function proxy(http) {
      // For transport close: you need const io = require('socket.io')(http, { pingInterval: 100, pingTimeout: 4000 });
